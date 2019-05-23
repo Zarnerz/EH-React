69ced51f3f55c6752309ec5194ad822cf2a64970
Dale Holstein <holstein30@live.com>
2019-05-23 16:54:56 -0400

    basic footer added - no styling

---
+++ esports-history/src/global/footer/Footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>&copy; 2019 Esports History</p>
+        <ul>
+          <li>
+            <a href="#">Email</a>
+          </li>
+          <li>
+            <a href="#">LinkedIn</a>
+          </li>
+          <li>
+            <a href="#">Github</a>
+          </li>
+          <li>
+            <a href="#">Twitter</a>
+          </li>
+        </ul>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ esports-history/src/global/footer/index.js
@@ -0,0 +1,3 @@
+// Export Footer
+import Footer from "./Footer";
+export default Footer;
