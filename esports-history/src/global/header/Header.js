3cab68016f40c314c7d5b19efd5017967fff9d8a
Dale Holstein <holstein30@live.com>
2019-05-23 16:44:49 -0400

    base navbar html complete

--- esports-history/src/global/header/Header.js
+++ esports-history/src/global/header/Header.js
@@ -9,6 +9,23 @@ class Header extends Component {
           <a href="#home">
             <img src={logo} alt="esports history logo" />
           </a>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#background">Background</a>
+            </li>
+            <li>
+              <a href="#podcast">Podcast</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
         </nav>
       </header>
     );
