5a170caa98bdd707c1f34c463b58625e49693a73
Dale Holstein <holstein30@live.com>
2019-05-25 18:07:12 -0400

    Footer styles added - inline styles utilized

--- esports-history/src/global/footer/Footer.js
+++ esports-history/src/global/footer/Footer.js
@@ -3,19 +3,19 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <p>&copy; 2019 Esports History</p>
-        <ul>
-          <li>
+      <footer style={footerStyle}>
+        <p style={footerCopyrightStyle}>&copy; 2019 Esports History</p>
+        <ul style={footerUlStyle}>
+          <li style={liStyle}>
             <a href="#">Email</a>
           </li>
-          <li>
+          <li style={liStyle}>
             <a href="#">LinkedIn</a>
           </li>
-          <li>
+          <li style={liStyle}>
             <a href="#">Github</a>
           </li>
-          <li>
+          <li style={liStyle}>
             <a href="#">Twitter</a>
           </li>
         </ul>
@@ -23,5 +23,26 @@ class Footer extends Component {
     );
   }
 }
+
+const footerStyle = {
+  display: "flex"
+};
+
+const footerCopyrightStyle = {
+  flex: "1",
+  textAlign: "left"
+};
+
+const footerUlStyle = {
+  flex: "1",
+  display: "flex",
+  justifyContent: "space-around",
+  alignSelf: "flex-end"
+};
+
+const liStyle = {
+  display: "inline",
+  textDecoration: "none"
+};
 
 export default Footer;
