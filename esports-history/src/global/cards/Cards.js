325b66510cea6b37b8b060bd077e9b42970fba77
Dale Holstein <holstein30@live.com>
2019-05-30 15:10:47 -0400

    Card images resized

--- esports-history/src/global/cards/Cards.js
+++ esports-history/src/global/cards/Cards.js
@@ -4,12 +4,20 @@ class Cards extends Component {
   render() {
     return (
       <section>
-        <img src={this.props.image} alt={this.props.alt} />
+        <img
+          style={cardImageSource}
+          src={this.props.image}
+          alt={this.props.alt}
+        />
         <h1>{this.props.title}</h1>
         <p>{this.props.blurb}</p>
       </section>
     );
   }
 }
+
+const cardImageSource = {
+  maxWidth: "300px"
+};
 
 export default Cards;
