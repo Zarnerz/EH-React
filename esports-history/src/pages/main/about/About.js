e70382fa05ae85a36b68258778d506579f4ddc9c
Dale Holstein <holstein30@live.com>
2019-06-02 11:53:58 -0400

    global line height added - about section paragraphs padding added to space better

--- esports-history/src/pages/main/about/About.js
+++ esports-history/src/pages/main/about/About.js
@@ -7,7 +7,7 @@ class About extends Component {
         <h1 style={h1Style}>Welcome to Esports History</h1>
         <article style={articleStyle}>
           <h1>Our Mission</h1>
-          <p>
+          <p style={articleParagraphStyle}>
             The mission of Esports History is to create a centralized place for
             the rich history of Esports. To research and document as much of
             this history as possible and bring it to fans both new and old.
@@ -15,7 +15,7 @@ class About extends Component {
         </article>
         <article style={articleStyle}>
           <h1>Our Story</h1>
-          <p>
+          <p style={articleParagraphStyle}>
             In 2019 Esports history was born after years of toying around with
             the idea. We had a small launch and committed ourselves to putting
             out the best content we could while covering as many topics as
@@ -40,6 +40,10 @@ const sectionStyle = {
 
 const articleStyle = {
   flex: "1"
+};
+
+const articleParagraphStyle = {
+  padding: "0 2rem"
 };
 
 const h1Style = {
