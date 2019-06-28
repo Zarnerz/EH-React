07d454d3c378073380dd5f3a16047e3d1fae6136
Dale Holstein <holstein30@live.com>
2019-06-28 14:57:09 -0400

    Player component functioning

--- esports-history/src/global/player/Player.js
+++ esports-history/src/global/player/Player.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 export class Player extends Component {
   render() {
-    return <audio controls src="https://player.fm/series/1335480/236881937" />;
+    return (
+      <audio
+        controls
+        src="http://hwcdn.libsyn.com/p/8/a/f/8af468c57789b632/Jocko_Podcast_183_JackCarr.mp3?c_id=45639680&cs_id=45639680&destination_id=454905&expiration=1561759910&hwt=b2d2d27b4bf8e3e5cdaa6d47929b50a0"
+      />
+    );
   }
 }
 
