5c7c2ed2c86fa72ec00c550475084bb238f0f258
Dale Holstein <holstein30@live.com>
2019-06-28 14:53:57 -0400

    Player component - removed all but base audio element - not functioning

--- esports-history/src/global/player/Player.js
+++ esports-history/src/global/player/Player.js
@@ -1,25 +1,8 @@
 import React, { Component } from "react";
 
 export class Player extends Component {
-  state = {
-    selectedTrack: null
-  };
-
   render() {
-    const list = [
-      { id: 1, title: "Introduction" },
-      { id: 2, title: "Final Boss" }
-    ].map(item => {
-      return (
-        <li
-          key={item.id}
-          onClick={() => this.setState({ selectedTrack: item.title })}
-        >
-          {item.title}
-        </li>
-      );
-    });
-    return <ul>{list}</ul>;
+    return <audio controls src="https://player.fm/series/1335480/236881937" />;
   }
 }
 
