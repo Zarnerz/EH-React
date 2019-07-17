5ee1a6854512f52c35c5483f6761d8d1bb7205d1
Dale Holstein <holstein30@live.com>
2019-07-16 20:17:30 -0400

    fixed Blog index

--- esports-history/src/App.js
+++ esports-history/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+
+// Global Components
 import Header from "./global/header";
 import Footer from "./global/footer";
+
+// Pages
 import Main from "./pages/main";
 import Podcasts from "./pages/podcasts";
+
+// Styles
 import "./App.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
--- esports-history/src/pages/blog/index.js
+++ esports-history/src/pages/blog/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import Player from "./player";
+import Article from "./article";
 
-class Podcasts extends Component {
+class Blog extends Component {
   render() {
     return (
       <main>
-        <Player source="http://hwcdn.libsyn.com/p/8/a/f/8af468c57789b632/Jocko_Podcast_183_JackCarr.mp3?c_id=45639680&cs_id=45639680&destination_id=454905&expiration=1561759910&hwt=b2d2d27b4bf8e3e5cdaa6d47929b50a0" />
+        <Article />
       </main>
     );
   }
 }
 
-export default Podcasts;
+export default Blog;
