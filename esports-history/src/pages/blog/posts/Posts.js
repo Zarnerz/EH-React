68b7b1a9459322acba627efd6c7a84aa3271f6a6
Dale Holstein <holstein30@live.com>
2019-07-20 12:22:59 -0400

    state added for Posts - trying to render a list of available articles/posts

--- esports-history/src/pages/blog/posts/Posts.js
+++ esports-history/src/pages/blog/posts/Posts.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
 export class Posts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [
+        { title: "title1", content: "content1" },
+        { title: "title2", content: "content2" },
+        { title: "title3", content: "content3" }
+      ]
+    };
+  }
+
+  renderArticles = () => {
+    this.state.articles.forEach(article => {
+      console.log(article);
+      return (
+        <table>
+          <tr>
+            <th>{article.title}</th>
+          </tr>
+          <tr>
+            <td>{article.content}</td>
+          </tr>
+        </table>
+      );
+    });
+  };
+
   render() {
-    return <div>Posts</div>;
+    return <main>{this.renderArticles}</main>;
   }
 }
 
