b976cb2efde07e54de64b138a0bdac7b58384bc7
Dale Holstein <holstein30@live.com>
2019-09-09 00:31:32 -0400

    build

---
+++ esports-history/build/precache-manifest.a4d5b5a24092b011079235c42be86505.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1fbd088c87c13d8a4b3223ba84f586",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4b150587e1f97fbdfdd",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "d18d7fb2462a995967bd",
+    "url": "/static/css/main.ec587069.chunk.css"
+  },
+  {
+    "revision": "a4b150587e1f97fbdfdd",
+    "url": "/static/js/2.2d141aab.chunk.js"
+  },
+  {
+    "revision": "d18d7fb2462a995967bd",
+    "url": "/static/js/main.be212870.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e87a9a25946110e43b99fd0979a41cd5",
+    "url": "/static/media/1.e87a9a25.jpg"
+  },
+  {
+    "revision": "c88e1a5d2665f581c2cb5af31aa1d2e0",
+    "url": "/static/media/13 - MLG FB MONEY.c88e1a5d.jpg"
+  },
+  {
+    "revision": "faf87aeaf9e6ad030cbc077fecf0e5e1",
+    "url": "/static/media/14 - MLG FB 2.faf87aea.jpg"
+  },
+  {
+    "revision": "c70fed30ca7163caea4e9d3d98aa34d5",
+    "url": "/static/media/15 - Smash Bros.c70fed30.jpg"
+  },
+  {
+    "revision": "080891ab46d30648e13d996f554c81ca",
+    "url": "/static/media/17 - SC1.080891ab.jpg"
+  },
+  {
+    "revision": "525527a5475daad39712f653fd84f9e4",
+    "url": "/static/media/19 - WCG.525527a5.jpg"
+  },
+  {
+    "revision": "dae411870574530bf28ed6f72523efd0",
+    "url": "/static/media/2.dae41187.jpg"
+  },
+  {
+    "revision": "2109e5c54f770c7c65cd33cfc0e05815",
+    "url": "/static/media/22 - Evo 2004.2109e5c5.jpg"
+  },
+  {
+    "revision": "52499fa18418c30abce672781686332a",
+    "url": "/static/media/27 - Twin Galaxies.52499fa1.jpg"
+  },
+  {
+    "revision": "8b74ae7d6c6f0bbdbbd28a51d0b9b65b",
+    "url": "/static/media/3.8b74ae7d.jpg"
+  },
+  {
+    "revision": "c20868482f6e8c05feff4fbf71081e09",
+    "url": "/static/media/6 - Life.c2086848.jpg"
+  },
+  {
+    "revision": "c021f1cd7c419c4dd0011d5c3adaad45",
+    "url": "/static/media/8 - Dendi Yell.c021f1cd.jpg"
+  },
+  {
+    "revision": "ebce20f6fb5f91ae6e35887a83864f71",
+    "url": "/static/media/Haike-Regular.ebce20f6.ttf"
+  },
+  {
+    "revision": "39e60d7bf555f9c62868268f2a98b36e",
+    "url": "/static/media/Haike-ThinReg.39e60d7b.ttf"
+  },
+  {
+    "revision": "b0c77a56944e31bf421ec13b3fbd6b35",
+    "url": "/static/media/Haike-boldreg.b0c77a56.ttf"
+  },
+  {
+    "revision": "bdaca416d455264e646814848ee8a246",
+    "url": "/static/media/Haike-col.bdaca416.ttf"
+  },
+  {
+    "revision": "e0e917f3209035bc37882d5235b5fc1a",
+    "url": "/static/media/Haike-condensedreg.e0e917f3.ttf"
+  },
+  {
+    "revision": "92339ee0b5016accb9cb65787c60e628",
+    "url": "/static/media/Sabo-Filled.92339ee0.otf"
+  },
+  {
+    "revision": "7116b3142d40bc9fc69e1740ca752e14",
+    "url": "/static/media/hamburgerMenu.7116b314.svg"
+  }
+]);
